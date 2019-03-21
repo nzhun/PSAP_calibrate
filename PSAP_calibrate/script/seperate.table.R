05c8b14fd0ffc14ec8d1613e623c350286e6dec5
nzhun <nz2274@cumc.columbia.edu>
2019-03-21 17:46:57 -0400

    remove PSAP_calibrate_subfolder

--- PSAP_calibrate/script/seperate.table.R
+++
@@ -1,9 +0,0 @@
-setwd("/home/local/ARCS/nz2274/PSAP_Setup")
-#setwd("~/")
-file="data/gencode.v27.simulation.gnomad_CADD13.table.txt";
-tab<-read.table(file,header=1,stringsAsFactors = F)
-genes<-unique(tab$Gene)
-for(i in seq(1,length(genes),by = 50)){
-  sets<-genes[i:min((i+49),length(genes))]
-  write.table(tab[which(tab$Gene%in%sets),],row.names = F,file = paste("data/temp/gencode.v27.simulation.gnomad_CADD13.table.",i,".txt",sep=""),quote = F,sep="\t")
-}
